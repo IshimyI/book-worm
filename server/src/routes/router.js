d1841901c6489bd47f8c6ff5cb4f00c158449739
IshimyI <ivan.borisenko.2003@mail.ru>
2025-02-27 22:49:30 +0300

    fix router

--- server/src/routes/router.js
+++ server/src/routes/router.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const { User, Book } = require("../../db/models");
-const { where } = require("sequelize");
+const { User, Book, Review } = require("../../db/models");
+const { Sequelize } = require("sequelize");
 
 const router = express.Router();
 
@@ -39,27 +39,25 @@ router.post("/book/new", async (req, res) => {
     return res.status(400).json({ message: "Поля должны быть заполнены" });
   }
 
-  const transaction = await Sequelize.transaction();
-
   try {
-    const [newBook] = await Book.findOrCreate({
+    const [newBook, created] = await Book.findOrCreate({
       where: { title, author },
       defaults: { genre, year, annotation, img },
-      transaction,
     });
 
-    if (user_rating) {
-      await Review.findOrCreate({
-        where: { book_id: newBook.id, user_id },
-        defaults: { body, user_rating },
-        transaction,
-      });
+    const [review, reviewCreated] = await Review.findOrCreate({
+      where: { book_id: newBook.id, user_id },
+      defaults: { body, user_rating },
+    });
+
+    if (!reviewCreated) {
+      review.body = body;
+      review.user_rating = user_rating;
+      await review.save();
     }
 
-    await transaction.commit();
-    res.status(201).json(newBook);
+    res.status(200).json({ book: newBook, review });
   } catch (error) {
-    await transaction.rollback();
     console.error(error);
     res.status(500).send("Ошибка при добавлении книги или отзыва");
   }
