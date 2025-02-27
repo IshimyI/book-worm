da2588025a5ff8caa192d39e44357abe5015f3b4
Maria Rodionova <mbelovaa15@gmail.com>
2025-02-27 10:55:44 +0300

    not comment

--- client/src/ui/BookModal.jsx
+++ client/src/ui/BookModal.jsx
@@ -1,9 +1,21 @@
-import { Button, Modal, ModalOverlay, ModalContent, ModalBody, ModalFooter, ModalCloseButton, Text, Image } from "@chakra-ui/react";
-
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ModalCloseButton,
+  Text,
+  Image,
+  Flex,
+  Box
+} from "@chakra-ui/react";
 
 const BookModal = ({ book, isOpen, onClose }) => {
   if (!book) {
-    return null; 
+    return null;
   }
   const OverlayOne = () => (
     <ModalOverlay
@@ -12,25 +24,36 @@ const BookModal = ({ book, isOpen, onClose }) => {
     />
   );
   return (
-    <Modal isCentered isOpen={isOpen} onClose={onClose}>
-      <OverlayOne />
-      <ModalContent>
+    <Modal isCentered isOpen={isOpen} onClose={onClose} size="xl">
+      <ModalOverlay />
+      <ModalContent maxW="1400px" minH="700px" p="6">
         <ModalCloseButton />
+        <ModalHeader fontSize="2xl" fontWeight="bold">
+          {book.title}
+        </ModalHeader>
         <ModalBody>
-        <Image 
-            src={"./default.jpg"} 
-            boxSize="200px" 
-            objectFit="cover"
-            borderRadius="md"
-            mb="4"
-          />
-          <Text>{book.title}</Text>
-          <Text>Автор: {book.author}</Text>
-          <Text>Жанр: {book.genre}</Text>
-          <Text>Год: {book.year}</Text>
-          <Text>Рейтинг: {book.rating}</Text>
-          <Text>Оценили: {book.quantity_rate}</Text>
-          <Text>{book.annotation}</Text>
+          <Flex direction={{ base: "column", md: "row" }} gap="6">
+          <Box flexShrink={0}>
+          <Image
+              src={book.IMG || "./default.jpg"}
+              width="250px"
+                height="350px"
+              objectFit="cover"
+              borderRadius="md"
+            />
+            </Box>
+            <Flex direction="column" flex="1">
+              <Text fontSize="lg">Автор: {book.author}</Text>
+              <Text fontSize="lg">Жанр: {book.genre}</Text>
+              <Text fontSize="lg">Год: {book.year}</Text>
+              <Text fontSize="lg">Рейтинг: {book.rating}</Text>
+              <Text fontSize="lg">Оценили: {book.quantity_rate}</Text>
+              {/* <Text mt="4">{book.annotation}</Text> */}
+              </Flex>
+          </Flex>
+          <Box mt="6">
+          <Text fontSize="md">{book.annotation}</Text>
+          </Box>
         </ModalBody>
         <ModalFooter>
           <Button onClick={onClose}>Закрыть</Button>
