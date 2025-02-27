281d7162a5a71cba9bab667403cd56b18d24ee5a
Maria Rodionova <mbelovaa15@gmail.com>
2025-02-27 13:08:31 +0300

    review

--- client/src/ui/BookModal.jsx
+++ client/src/ui/BookModal.jsx
@@ -10,13 +10,34 @@ import {
   Text,
   Image,
   Flex,
-  Box
+  Box,
+  Divider,
+  Stack,
+  Avatar
 } from "@chakra-ui/react";
 
 const BookModal = ({ book, isOpen, onClose }) => {
   if (!book) {
     return null;
   }
+
+  const testReviews = [
+    {
+      user: "Иван Петров",
+      text: "Отличная книга! Очень понравилась глубина персонажей.",
+      avatarUrl: "https://bit.ly/broken-link"
+    },
+    {
+      user: "Мария Сидорова",
+      text: "Интересный сюжет, но мне не хватило динамики в развитии событий.",
+      avatarUrl: "https://bit.ly/broken-link"
+    },
+    {
+      user: "Александр Иванов",
+      text: "Читал давно, но до сих пор под впечатлением. Рекомендую!",
+      avatarUrl: "https://bit.ly/broken-link"
+    },
+  ];
   const OverlayOne = () => (
     <ModalOverlay
       bg="blackAlpha.300"
@@ -25,7 +46,7 @@ const BookModal = ({ book, isOpen, onClose }) => {
   );
   return (
     <Modal isCentered isOpen={isOpen} onClose={onClose} size="xl">
-      <ModalOverlay />
+      <OverlayOne />
       <ModalContent maxW="1400px" minH="700px" p="6">
         <ModalCloseButton />
         <ModalHeader fontSize="2xl" fontWeight="bold">
@@ -33,27 +54,67 @@ const BookModal = ({ book, isOpen, onClose }) => {
         </ModalHeader>
         <ModalBody>
           <Flex direction={{ base: "column", md: "row" }} gap="6">
-          <Box flexShrink={0}>
-          <Image
-              src={book.IMG || "./default.jpg"}
-              width="250px"
+            <Box flexShrink={0}>
+              <Image
+                src={book.IMG || "./default.jpg"}
+                width="250px"
                 height="350px"
-              objectFit="cover"
-              borderRadius="md"
-            />
+                objectFit="cover"
+                borderRadius="md"
+              />
+              <Box mt="4">
+                <Text fontSize="lg">Автор: {book.author}</Text>
+                <Text fontSize="lg">Жанр: {book.genre}</Text>
+                <Text fontSize="lg">Год: {book.year}</Text>
+                <Text fontSize="lg">Рейтинг: {book.rating}</Text>
+                <Text fontSize="lg">Оценили: {book.quantity_rate}</Text>
+                {/* <Text mt="4">{book.annotation}</Text> */}
+              </Box>
             </Box>
-            <Flex direction="column" flex="1">
-              <Text fontSize="lg">Автор: {book.author}</Text>
-              <Text fontSize="lg">Жанр: {book.genre}</Text>
-              <Text fontSize="lg">Год: {book.year}</Text>
-              <Text fontSize="lg">Рейтинг: {book.rating}</Text>
-              <Text fontSize="lg">Оценили: {book.quantity_rate}</Text>
-              {/* <Text mt="4">{book.annotation}</Text> */}
-              </Flex>
+
+            <Box flex="2" maxW="400px">
+              <Text fontSize="md" noOfLines={10} overflow="hidden">
+                {book.annotation}
+              </Text>
+            </Box>
+            <Box flex="1">
+              <Text fontSize="xl" fontWeight="bold" mb="4">
+                Рецензии
+              </Text>
+              <Box
+                maxH="400px"
+                overflowY="auto"
+                p="2"
+                border="1px solid #ccc"
+                borderRadius="md"
+              >
+                {(book.reviews && book.reviews.length > 0
+                  ? book.reviews
+                  : testReviews
+                ).map((review, index) => (
+                  <Box
+                    key={index}
+                    p="4"
+                    border="1px solid #ddd"
+                    borderRadius="md"
+                    mb="4"
+                  >
+                    <Stack direction="row" spacing="4" align="center">
+                    <Avatar name={review.user} src={review.avatarUrl} />
+                    <Box>
+                    <Text fontWeight="bold">{review.user}</Text>
+                    <Divider my="2" />
+                    <Text>{review.text}</Text>
+                  </Box>
+                  </Stack>
+                  </Box>
+                ))}
+                {book.reviews && book.reviews.length === 0 && (
+                  <Text color="gray.500">Нет рецензий на эту книгу.</Text>
+                )}
+              </Box>
+            </Box>
           </Flex>
-          <Box mt="6">
-          <Text fontSize="md">{book.annotation}</Text>
-          </Box>
         </ModalBody>
         <ModalFooter>
           <Button onClick={onClose}>Закрыть</Button>
