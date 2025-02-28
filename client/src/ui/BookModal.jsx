8f5a27fed8297b53d320d828d4403ff51878f076
DmitryBr32 <ipatdmitry@gmail.com>
2025-02-28 16:36:20 +0300

    MooodallkiiiMl

--- client/src/ui/BookModal.jsx
+++ client/src/ui/BookModal.jsx
@@ -1,34 +1,16 @@
 /* eslint-disable react/prop-types */
-import {
-  Button,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalCloseButton,
-  Text,
-  Image,
-  Flex,
-  Box,
-  Divider,
-  Stack,
-  Avatar,
-  Textarea,
-  Heading,
-  Input
-} from "@chakra-ui/react";
-import { StarIcon, EditIcon } from "@chakra-ui/icons";
-import { useState } from "react";
-import axiosInstance from "../axiosInstance";
+import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton, Text, Image, Flex, Box, Divider, Stack, Avatar, Textarea, Heading, Input } from '@chakra-ui/react';
+import { StarIcon, EditIcon } from '@chakra-ui/icons';
+import { useState } from 'react';
+import axiosInstance from '../axiosInstance';
 
 const BookModal = ({ book, isOpen, onClose, user }) => {
   //const [newReview, setNewReview] = useState("");
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
-  const [textInput, setTextInput] = useState("");
+  const [textInput, setTextInput] = useState('');
   const [click, setClick] = useState(false);
-  const [inputBody, setInputBody] = useState('')
+  const [inputBody, setInputBody] = useState('');
 
   // const [reviews, setReviews] = useState([
   // {
@@ -53,10 +35,10 @@ const BookModal = ({ book, isOpen, onClose, user }) => {
       const newRev = {
         user: user.name,
         text: newReview,
-        avatarUrl: "https://bit.ly/broken-link",
+        avatarUrl: 'https://bit.ly/broken-link',
       };
       setReviews([...reviews, newRev]);
-      setNewReview("");
+      setNewReview('');
     }
   };
   const handleRating = (value) => {
@@ -73,33 +55,30 @@ const BookModal = ({ book, isOpen, onClose, user }) => {
   };
 
   const handleFavorites = async () => {
-    console.log("Данные на сервер:", {
+    console.log('Данные на сервер:', {
       user_id: user.id,
       book_id: book.id,
     });
     try {
-      const res = await axiosInstance.post(
-        `http://localhost:3000/api/updateFavourites/${user.id}`,
-        {
-          bookId: book.id,
-        }
-      );
-      console.log("Ответ от сервера:", res.data);
+      const res = await axiosInstance.post(`http://localhost:3000/api/updateFavourites/${user.id}`, {
+        bookId: book.id,
+      });
+      console.log('Ответ от сервера:', res.data);
       if (res.status === 200) {
-        console.log("Книга добавлена в избранное");
+        console.log('Книга добавлена в избранное');
       } else {
-        console.log("Что-то пошло не так");
+        console.log('Что-то пошло не так');
       }
     } catch (error) {
-      console.log("Ошибка при добавлении книги в избранное:", error);
+      console.log('Ошибка при добавлении книги в избранное:', error);
     }
   };
 
   const addReviewHandler = async (event) => {
     event.preventDefault();
-    console.log("инпут боди", inputBody);
+    console.log('инпут боди', inputBody);
     if (!inputBody) {
-      console.log("Поля должны быть заполнены");
+      console.log('Поля должны быть заполнены');
     } else {
       try {
         const res = await axiosInstance.post(`/book/new`, {
@@ -115,7 +94,7 @@ const BookModal = ({ book, isOpen, onClose, user }) => {
           setHover(0);
         }
       } catch (error) {
-        console.log(error, "что-то c add не так");
+        console.log(error, 'что-то c add не так');
       }
     }
   };
@@ -124,18 +103,12 @@ const BookModal = ({ book, isOpen, onClose, user }) => {
     return null;
   }
 
-  const OverlayOne = () => (
-    <ModalOverlay
-      bg="blackAlpha.300"
-      backdropFilter="blur(10px) hue-rotate(90deg)"
-    />
-  );
+  const OverlayOne = () => <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(10px) hue-rotate(90deg)" />;
 
   const changeHandler = (event) => {
     event.preventDefault();
     setInputBody(() => event.target.value);
     setRating(() => event.target.value);
-
   };
 
   const switchClick = () => {
@@ -143,16 +116,16 @@ const BookModal = ({ book, isOpen, onClose, user }) => {
   };
 
   async function remakeHandler(event) {
-    console.log("put отработал");
+    console.log('put отработал');
     event.preventDefault();
     try {
       await axiosInstance.put(`/api/review/${review.id}`, {
         body: textInput,
       });
       setClick(true);
-      setTextInput("");
+      setTextInput('');
     } catch (error) {
-      console.log(error, "что-то c del не так");
+      console.log(error, 'что-то c del не так');
       setClick(false);
     }
   }
@@ -170,30 +143,11 @@ const BookModal = ({ book, isOpen, onClose, user }) => {
             <Box>
               <Flex className="this12">
                 <Box maxWidth="250px">
-                  <Image
-                    src={book.img || "./default.jpg"}
-                    width="250px"
-                    height="350px"
-                    objectFit="cover"
-                    borderRadius="md"
-                  />
+                  <Image src={book.img || './default.jpg'} width="250px" height="350px" objectFit="cover" borderRadius="md" />
                 </Box>
-                <Box
-                  w="65%"
-                  mr={"20px"}
-                  p={4}
-                  borderWidth="1px"
-                  borderRadius="md"
-                  boxShadow="sm"
-                  bg="white"
-                >
+                <Box w="65%" mr={'20px'} p={4} borderWidth="1px" borderRadius="md" boxShadow="sm" bg="white">
                   <Stack spacing={3}>
-                    <Heading
-                      as="h3"
-                      size="md"
-                      fontWeight="semibold"
-                      color="gray.700"
-                    >
+                    <Heading as="h3" size="md" fontWeight="semibold" color="gray.700">
                       Информация о книге:
                     </Heading>
                     <Box>
@@ -229,69 +183,25 @@ const BookModal = ({ book, isOpen, onClose, user }) => {
                   </Stack>
                 </Box>
               </Flex>
-              <Box mt="20px" pr={"25px"} mb={"25px"}>
+              <Box mt="20px" pr={'25px'} mb={'25px'}>
                 <Text fontSize="md">{book.annotation}</Text>
-              </Box>{" "}
+              </Box>{' '}
             </Box>
             <Box>
-              <Box w={"60vh"}>
+              <Box w={'60vh'}>
                 <Text fontSize="xl" fontWeight="bold" mb="4">
                   Рецензии
                 </Text>
-                <Box
-                  maxH="300px"
-                  overflowY="auto"
-                  p="2"
-                  border="1px solid #ccc"
-                  borderRadius="md"
-                >
-                  {(book.review && book.review.length > 0
-                    ? book.review
-                    : []
-                  ).map((review, index) => {
+                <Box maxH="300px" overflowY="auto" p="2" border="1px solid #ccc" borderRadius="md">
+                  {(book.reviews && book.reviews.length > 0 ? book.reviews : []).map((review, index) => {
                     return (
-                      <Box
-                        key={index}
-                        p="3"
-                        border="1px solid #ddd"
-                        borderRadius="md"
-                        mb="3"
-                      >
+                      <Box key={index} p="3" border="1px solid #ddd" borderRadius="md" mb="3">
                         <Stack direction="row" spacing="4" align="center">
-                          <Avatar
-                            name={review.userName}
-                            src={`https://bit.ly/broken-link`}
-                          />
+                          <Avatar name={review.userName} src={`https://bit.ly/broken-link`} />
 
                           <Box>
                             <Flex justifyContent="space-between">
                               <Text fontWeight="bold">{review.userName}</Text>
-                              <Box>
-                                {user.id && (
-                                  <>
-                                    {click ? (
-                                      <Button
-                                        onClick={switchClick}
-                                      >
-                                        <EditIcon />
-                                      </Button>
-                                    ) : (
-                                      <form onSubmit={remakeHandler}>
-                                        <Input
-                                          value={newReview}
-                                          onChange={(e) =>
-                                            setNewReview(e.target.value)
-                                          }
-                                          placeholder="Напиши свою рецензию"
-                                        />
-                                        <Button type="submit">
-                                          ОК
-                                        </Button>
-                                      </form>
-                                    )}
-                                  </>
-                                )}
-                              </Box>
                             </Flex>
                             <Divider my="2" />
                             <Text>{review.user_rev}</Text>
@@ -306,11 +216,7 @@ const BookModal = ({ book, isOpen, onClose, user }) => {
                   )} */}
                 </Box>
                 <Box mt="4">
-                  <Textarea
-                    value={inputBody}
-                    onChange={(e) => setInputBody(e.target.value)}
-                    placeholder="Напиши свою рецензию"
-                  />
+                  <Textarea value={inputBody} onChange={(e) => setInputBody(e.target.value)} placeholder="Напиши свою рецензию" />
 
                   <Box mt="15px">
                     {[...Array(5)].map((_, index) => {
@@ -321,11 +227,7 @@ const BookModal = ({ book, isOpen, onClose, user }) => {
                           aria-label={`Рейтинг ${ratingValue}`}
                           fontSize="25px"
                           variant="ghost"
-                          color={
-                            ratingValue <= (hover || rating)
-                              ? "gold"
-                              : "gray.300"
-                          }
+                          color={ratingValue <= (hover || rating) ? 'gold' : 'gray.300'}
                           onClick={() => handleRating(ratingValue)}
                           onMouseEnter={() => handleMouseEnter(ratingValue)}
                           onMouseLeave={handleMouseLeave}
@@ -338,19 +240,11 @@ const BookModal = ({ book, isOpen, onClose, user }) => {
             </Box>
           </Flex>
 
-          <Flex className="this123" mt={"30px"}>
-            <Button
-              backgroundColor="#909e18"
-              color="white"
-              onClick={handleFavorites}
-            >
+          <Flex className="this123" mt={'30px'}>
+            <Button backgroundColor="#909e18" color="white" onClick={handleFavorites}>
               Добавить в избранное
             </Button>
-            <Button
-              backgroundColor="#334d00"
-              color="white"
-              onClick={addReviewHandler}
-            >
+            <Button backgroundColor="#334d00" color="white" onClick={addReviewHandler}>
               Добавить рецензию
             </Button>
           </Flex>
